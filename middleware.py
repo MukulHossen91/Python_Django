fba3e8bab83770aa82d975dad1a15e1d8ad7716c
Mukul Hossen <137640824+MukulHossen91@users.noreply.github.com>
2023-10-22 12:35:41 +0600

    Delete middleware.py

--- middleware.py
+++
@@ -1,8 +0,0 @@
-def first_middleware(get_response):
-    print("One time initialization")
-    def first_func(request):
-        print("This is before view")
-        response = get_response(request)
-        print("This is after view")
-        return response
-    return first_func
